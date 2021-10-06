0b2b0ef454f1b00b33ca198b662f143b6b3cb3cb
ridatuljannah0112 <ridhaatulljannah12@gmail.com>
2021-10-06 10:55:36 +0800

    First Commit : Praktik Video 3

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Belajar Flutter',
+      theme: ThemeData(primarySwatch: Colors.red),
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          onPressed: () {},
+        ),
+        backgroundColor: Colors.blue,
+        title: Text(
+          'Hello AppBar', 
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        actions: [Icon(Icons.search), Icon(Icons.more_vert)],
+        centerTitle: true,
+      ),
+    );
+  }
+}
